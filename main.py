fafe7f8ff110184dedc4fa0db4c3a8a878e98dd1
Joshua <joshua@amplifycp.com>
2024-07-29 16:29:53 -0500

    setup deallocation and azure credentials

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from azure.identity import DefaultAzureCredential
+from azure.identity import ClientSecretCredential
 from azure.mgmt.compute import ComputeManagementClient
-from azure.mgmt.compute.models import VirtualMachine
 # import os for environment variables
 import os
 from dotenv import load_dotenv, dotenv_values
@@ -16,13 +15,18 @@ resource_group_name = os.getenv("ESL_RESOURCE_GROUP")
 vm_name = os.getenv("ESL_VM_NAME")
 
 # Initialize Azure credentials
-credential = DefaultAzureCredential()
+credential = ClientSecretCredential(
+    tenant_id=os.getenv('AZURE_TENANT_ID'),
+    client_id=os.getenv('AZURE_CLIENT_ID'),
+    client_secret=os.getenv('AZURE_CLIENT_SECRET')
+)
+
 
 # Initialize ComputeManagementClient
 compute_client = ComputeManagementClient(credential, subscription_id)
 
 # Stop the VM
-async_vm_stop = compute_client.virtual_machines.begin_power_off(resource_group_name, vm_name)
+async_vm_stop = compute_client.virtual_machines.begin_deallocate(resource_group_name, vm_name)
 async_vm_stop.wait()
 
 print(f"The VM {vm_name} has been stopped.")
